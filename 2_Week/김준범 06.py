ed791d8d9d96d557b06c1b46b49cf8a88aab5a89
junbeom <72601028+JunbeomKim-01@users.noreply.github.com>
2022-09-12 18:40:00 +0900

    2022-09-12_P-7_01

--- 2_Week/김준범 06.py
+++ 2_Week/김준범 06.py
@@ -1,28 +1,32 @@
 # 이중 for문 -> 메모리 초과 -> O(n) 알고리즘으로 문제를 해결해야함 -> 투포인터 알고리즘
 import sys
 input = sys.stdin.readline
-N = int(input())
-array = [x for x in range(N+1)]
-strat_index = 1
-end_index = 1
-result = 1
-_sum = 1
-# 투포인터 이동
-# sum > N -> start index ++, sum - array[start_index]
-# sum < N -> end index ++ , sum + array[end_index]
-# sum == N -> count ++, end index ++ , sum + array[end_index]
-while end_index != N:
-    if _sum == N:
-        result += 1
-        end_index += 1
-        _sum += array[end_index]
-    elif _sum > N:
-        _sum -= array[strat_index]
-        strat_index += 1
-    else:
-        end_index += 1
-        _sum += array[end_index]
-print(result)
+def p_6(N):
+    N = int(N)
+    array = [x for x in range(N + 1)]
+    strat_index = 1
+    end_index = 1
+    result = 1
+    _sum = 1
+    # 투포인터 이동
+    # sum > N -> start index ++, sum - array[start_index]
+    # sum < N -> end index ++ , sum + array[end_index]
+    # sum == N -> count ++, end index ++ , sum + array[end_index]
+    while end_index != N:
+        if _sum == N:
+            result += 1
+            end_index += 1
+            _sum += array[end_index]
+        elif _sum > N:
+            _sum -= array[strat_index]
+            strat_index += 1
+        else:
+            end_index += 1
+            _sum += array[end_index]
+    print(result)
+if __name__ == '__main__':
+    N = input()
+    p_6(N)
 # 시간 초과된 알고리즘
 # sum_array = []
 # sum_array.append([sum(array[i:_+1])for _ in array]for i in range(N)) # 모든 경우의 구간합 배열
---
+++ 2_Week/김준범 07.py
@@ -0,0 +1,45 @@
+def p_7(N,M,array):
+    start_index = 0
+    end_index = int(N)-1
+    result = 0
+
+    def quick_sort(arr):
+        if len(arr) <= 1:
+            return arr
+        pivot = int(arr[len(arr) // 2])
+        lesser_arr, equal_arr, greater_arr = [], [], []
+        for num in arr:
+            if int(num) < pivot:
+                lesser_arr.append(int(num))
+            elif int(num) > pivot:
+                greater_arr.append(int(num))
+            else:
+                equal_arr.append(int(num))
+        return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
+
+    # def quick_sort(array):
+    #
+    #     if len(array) <= 1: return array
+    #     pivot, tail = int(array[0]), array[1:]
+    #     leftSide = [int(x) for x in tail if int(x) <= pivot]
+    #     rightSide = [int(x) for x in tail if int(x) > pivot]
+    #     return quick_sort(leftSide) + [pivot] + quick_sort(rightSide)
+
+    array_sroted = quick_sort(array)
+
+    while start_index < end_index:
+        if array_sroted[start_index] + array_sroted[end_index] == int(M) :
+            result += 1
+            start_index +=1
+            end_index -= 1
+        elif array_sroted[start_index] + array_sroted[end_index] < int(M) :
+            start_index +=1
+        else:
+            end_index -= 1
+    print(result)
+
+if __name__ == '__main__':
+    N = input()
+    M = input()
+    array = list(map(str,input().split()))
+    p_7(N,M,array)
