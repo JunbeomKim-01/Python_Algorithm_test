517f36bff4094eec16540e1180a7a47f6458cfa5
junbeom <72601028+JunbeomKim-01@users.noreply.github.com>
2022-09-19 15:33:02 +0900

    2022-09-19_add P-11_v1

---
+++ 3_Week/김준범 11.py
@@ -0,0 +1,26 @@
+def solve11(N):
+    stack = []
+    result = []
+    cur = 1
+    excep = False
+    for i in range(N):
+        n = int(input())
+        while cur <= n:
+            stack.append(cur)
+            result.append('+')
+            cur += 1
+        if stack[-1] == n:
+            stack.pop()
+            result.append('-')
+        else:
+            print('NO')
+            excep = True
+            break
+    if not excep:
+        for i in result:
+            print(i)
+
+
+N = 8
+
+solve11(N)
