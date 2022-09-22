74beec4e5c18eaa28916424fa3dd3538a0c8559e
junbeom <72601028+JunbeomKim-01@users.noreply.github.com>
2022-09-22 15:08:54 +0900

    2022-09-22_오큰수(17298)_v2

--- 3_Week/김준범 12.py
+++ 3_Week/김준범 12.py
@@ -1,8 +1,18 @@
 def solve12(N,A):
-    print(N)
-    print(A)
+    stack = []
+    for i in range(N-1):
+        n = i+1
+        while n < N:
+            if A[i] < A[n]:
+                stack.append(A[i+1])
+                break
+            elif i == N:
+                stack.append(-1)
+                break
+            n += 1
+    stack.append(-1)
+    print(stack)
 
-    pass
 if __name__ == '__main__':
     N = int(input())
     A = list(map(int,input().split()))
